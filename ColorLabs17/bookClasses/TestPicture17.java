3f555ef9aa45e633bc1b996eb07d495ed31fdd19
MichaelPark2002 <mjpark@stu.naperville203.org>
2019-10-28 15:08:37 -0500

    asdf

--- ColorLabs17/bookClasses/TestPicture17.java
+++ ColorLabs17/bookClasses/TestPicture17.java
@@ -64,21 +64,18 @@ public class TestPicture17
 			maxRed = spot.getRed();
 			spot.setRed(255);
 	}
-	pog1.explore();
 
 	for (Pixel spot2 : pixels2)
 		{
 			maxGreen = spot2.getGreen();
 			spot2.setGreen(255);
 		}
-	pog2.explore();
 
 	for (Pixel spot3 : pixels3)
 		{
 			maxBlue = spot3.getBlue();
 			spot3.setBlue(255);
 		}
-	pog3.explore();
 
 	//changes the picture to a negative
 	//opposite color 0 is 255, 1 is 254, 2 is 253 and so on
@@ -92,7 +89,6 @@ public class TestPicture17
 		//make opposite with range of 0 to 255.
 		spot4.setColor(new Color(255-redValue,255-greenValue,255-blueValue));
 	}
-	pog4.explore();
 
 	//changes the color by the factor given test at:
 	//red 50% = .5, green 150% = 1.5, blue 25% = .25
@@ -105,7 +101,6 @@ public class TestPicture17
 		value1 *= factor1;
 		pixelObj.setRed(value1);
     }
-    pog5.explore();
 
 	for (Pixel pixelObj : pixels6)
 	    {
@@ -114,7 +109,6 @@ public class TestPicture17
 			value2 *= factor2;
 			pixelObj.setGreen(value2);
 	    }
-    pog6.explore();
 
     for (Pixel pixelObj : pixels7)
 		    {
@@ -124,25 +118,95 @@ public class TestPicture17
 		        //set the red value of the current pixel to the new value
 				pixelObj.setBlue(value3);
 		    }
-    pog7.explore();
 
     //Grayscale
-    int gray1, gray2, gray3;
+    int gray1, gray2, gray3, result;
     for (Pixel spot8 : pixels8)
 			{
 				gray1 = spot8.getRed();
 				gray2 = spot8.getGreen();
 				gray3 = spot8.getBlue();
-				//make opposite with range of 0 to 255.
+				result = (gray1 + gray2 + gray3)/3;
+				gray1 = result;
+				gray2 = result;
+				gray3 = result;
 				spot8.setColor(new Color(gray1,gray2,gray3));
 			}
-	pog8.explore();
 
 	//Darken
+	int dark1 = 0;
+	int dark2 = 0;
+	int dark3 = 0;
+	for (Pixel pixelObj : pixels9)
+	        {
+				dark1 = pixelObj.getRed();
+				if (dark1 < 130)
+				{
+					pixelObj.setRed(50);
+				}
+				dark2 = pixelObj.getGreen();
+				if (dark2 < 130)
+				{
+					pixelObj.setGreen(50);
+				}
+				dark3 = pixelObj.getBlue();
+				if (dark1 < 130)
+				{
+					pixelObj.setBlue(50);
+				}
+	        }
 
-	//changes the color of each pixel in the picture object by
-	//amounts @param redAmount the amount to change the red value
+	//Colorify
+	int color1 = 0;
+	int color2 = 0;
+	int color3 = 0;
+	for (Pixel pixelObj : pixels10)
+	{
+				color1 = pixelObj.getRed();
+				if (color1 < 70 && color2 < 70 && color3 < 70)
+				{
+					pixelObj.setRed(255);
+					pixelObj.setGreen(100);
+					pixelObj.setBlue(100);
+				}
+	}
 
+	//Swap 2: Swap green to red
+	int swap1 = 0;
+	int swap2 = 0;
+	for (Pixel pixelObj : pixels11)
+	{
+		swap1 = pixelObj.getRed();
+		swap2 = pixelObj.getGreen();
+		pixelObj.setGreen(swap1);
+	}
+
+	//Swap 3: Swap red to green, green to blue, blue to red
+	int switch1 = 0;
+	int switch2 = 0;
+	int switch3 = 0;
+	for (Pixel pixelObj : pixels12)
+		{
+			switch1 = pixelObj.getRed();
+			switch2 = pixelObj.getGreen();
+			switch3 = pixelObj.getBlue();
+			pixelObj.setRed(switch2);
+			pixelObj.setGreen(switch3);
+			pixelObj.setBlue(switch1);
+		}
+	//Executions
+	pog12.explore();
+	pog11.explore();
+	pog10.explore();
+	pog9.explore();
+	pog8.explore();
+	pog7.explore();
+	pog6.explore();
+	pog5.explore();
+	pog4.explore();
+	pog3.explore();
+	pog2.explore();
+	pog1.explore();
     /*
         //access each index
     System.out.println(pixels[17]);
@@ -247,7 +311,7 @@ final double  FACTOR = .5;
 
   /**/
     //write/save a picture as a file
-    pog1.write("images/ferris11.jpg");
+    //pog1.write("images/ferris11.jpg");
 
     /**/
   }//main
